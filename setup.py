f0000fb9b357a56f7f2296e20c350b469ab34043
Andres Montano <andres.montano@tecnalia.com>
2024-10-22 17:01:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup
+import pathlib
 
-with open("README.md", "r") as fin:
-    long_description = fin.read()
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='am_rocker',
+    name='rocker_extensions',
     version='0.0.0',
 
     description='Extra plugins for rocker',
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    author='Andres Montano  ',
+    author='Andres Montano',
     author_email='andres.montano@tecnalia.com',
 
     packages=['rocker_extensions'],
